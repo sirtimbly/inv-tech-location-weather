0663792298c5a62155bc81597ac765cd03119a3c
Tim Bendt <sirtimbly@gmail.com>
2019-03-13 20:22:29 -0600

    chore(configuration): added dev deps for basic project, with necessary configs in package.json

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+
+// load sample data
+
+// check all zips against cache for location codes we've already fetched
+
+// make all requests for remaining location codes
+// https://developer.accuweather.com/accuweather-locations-api/apis/get/locations/v1/postalcodes/search
+
+// cache location code responses to disk
+
+// make all requests for weather current conditions and local time
+// https://developer.accuweather.com/accuweather-current-conditions-api/apis/get/currentconditions/v1/%7BlocationKey%7D
